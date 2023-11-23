a0d6647d1b36d2eecdcba37ffe40439c8f780595
Victor ANCLIN <victor.zano@gmail.com>
2023-11-23 14:54:16 +0100

    form test créer

--- database.sql
+++ database.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
--- http://www.phpmyadmin.net
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
 --
--- Client :  localhost
--- Généré le :  Jeu 26 Octobre 2017 à 13:53
--- Version du serveur :  5.7.19-0ubuntu0.16.04.1
--- Version de PHP :  7.0.22-0ubuntu0.16.04.1
+-- Hôte : 127.0.0.1:3306
+-- Généré le : jeu. 23 nov. 2023 à 11:12
+-- Version du serveur : 8.0.34
+-- Version de PHP : 8.0.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,47 +18,39 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `simple-mvc`
+-- Base de données : `sdm`
 --
+
+DROP DATABASE IF EXISTS `sdm`;
+CREATE DATABASE IF NOT EXISTS `sdm`;
+USE `sdm`;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `item`
+-- Structure de la table `sdm`
 --
 
-CREATE TABLE `item` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `title` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `sdm`;
+CREATE TABLE IF NOT EXISTS `sdm` (
+                                     `id` int NOT NULL AUTO_INCREMENT,
+                                     `pseudo` varchar(25) NOT NULL,
+                                     `title` varchar(50) NOT NULL,
+                                     `story` varchar(500) NOT NULL,
+                                     `photo` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+                                     `love` int NOT NULL,
+                                     `no_love` int NOT NULL,
+                                     PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Contenu de la table `item`
+-- Déchargement des données de la table `sdm`
 --
 
-INSERT INTO `item` (`id`, `title`) VALUES
-(1, 'Stuff'),
-(2, 'Doodads');
+INSERT INTO `sdm` (`id`, `pseudo`, `title`, `story`, `photo`, `love`, `no_love`) VALUES
+    (1, 'toto', 'Lorem', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum massa est, aliquet sit amet imperdiet nec, mattis vitae lectus. Vestibulum dapibus nisl ac dui gravida, quis efficitur augue rhoncus. Donec rutrum ornare volutpat. Integer turpis sem, mollis finibus placerat eget, laoreet posuere leo. In hendrerit urna ante. Vestibulum dui felis, aliquet eget suscipit sed, hendrerit eget ligula. Mauris a orci nec odio maximus placerat. Quisque faucibus, urna in lacinia porta, libero quam nullam.', NULL, 0, 0);
+COMMIT;
 
---
--- Index pour les tables exportées
---
-
---
--- Index pour la table `item`
---
-ALTER TABLE `item`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT pour les tables exportées
---
-
---
--- AUTO_INCREMENT pour la table `item`
---
-ALTER TABLE `item`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- src/Model/SDMManager.php
+++ src/Model/SDMManager.php
@@ -6,5 +6,24 @@ use App\Model\AbstractManager;
 
 class SDMManager extends AbstractManager
 {
+    public const TABLE = 'SDM';
 
+
+    public function insert(array $credentials)
+    {
+        $statement = $this->pdo->prepare(
+            "INSERT INTO " . static::TABLE .
+            "(`pseudo`, `title`, `story`, `photo`,`love`,`no_love`)
+            VALUES(:pseudo, :title, :story, :photo, :love, :no_love) "
+        );
+        $statement->bindValue(':pseudo', $credentials['pseudo'], \PDO::PARAM_STR);
+        $statement->bindValue(':title', $credentials['title'], \PDO::PARAM_STR);
+        $statement->bindValue(':story', $credentials['story'], \PDO::PARAM_STR);
+        $statement->bindValue(':photo', $credentials['photo'], \PDO::PARAM_STR);
+        $statement->bindValue(':love', $credentials['love'], \PDO::PARAM_INT);
+        $statement->bindValue(':no_love', $credentials['no_love'], \PDO::PARAM_INT);
+        //var_dump($credentials);
+        //die();
+        $statement->execute();
+    }
 }
